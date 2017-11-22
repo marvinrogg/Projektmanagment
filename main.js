28ea4a0a60f43359616094fded24c76e8cf0edfd
marvinrogg <mrogg@hs-offenburg.de>
2017-11-22 16:12:40 +0100

    Grundgerüst Anfragen v1.1, Fehlerbehandlung eingefügt

--- main.js
+++ main.js
@@ -7,11 +7,43 @@ var Client = require('node-rest-client').Client;
 var client = new Client();
 var city ="Offenburg";
 
+/*
+//TEST Webseite erstellen
+var express =require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.get('/', function (req,res) {
+
+    res.render('index', { title: 'My cool page!'})
+
+});
+app.listen(3000);
+
+*/
+
+
 var args = {
     headers : {"Authorization" : "Bearer 207af2c9a411a2846e84fd7dd96d9c83"}
             };
 
- eva = getEva();
+
+
+//get Date
+
+var date = new Date();
+var year = date.getFullYear();
+year = year.toString().substr(-2);
+var month = date.getMonth()+1;
+var day = date.getDate();
+
+var yymmdd = year+""+month+""+day;
+var currenthours = date.getHours();
+
+console.log(yymmdd);
+
+
+eva = getEva();
 
 departure = getDeparture();
 
@@ -49,32 +81,35 @@ function getDeparture() {
 }
 */
 
+
+//Abfrage Haltestops, geplante Abfahrtszeit und geänderte Abfahrtszeit
+//aktuelles Problem, Reihenfolge der Darstellung und Error Handling
 function getDeparture(){
 
 
-client.get("https://api.deutschebahn.com/timetables/v1/plan/8000290/171121/12", args, function (data, response) {
+client.get("https://api.deutschebahn.com/timetables/v1/plan/8000290/171122/14", args, function (data, response) {
 
 
-       for(var i = 0; data.timetable.s[i].dp.$.ppth != null; i++) {
+       for(var i = 0; data.timetable.s[i].dp !== (undefined); i++) {
 
-          // if (data.timetable.s[i].dp.$ == undefined) {
-         //   i++;
-         //  } else {
+           if (data.timetable.s[i].dp.$ === undefined) {
+            i++;
+           } else {
 
 
 
            //Abfahrtszeit geplant (PlannedTime)
            console.log(data.timetable.s[i].dp.$.pt);
 
-           //Abfahrtszeit geändert (CurrentTime)
-           //console.log(data.timetable.s[i].dp.$.ct);
+           //Abfahrtszeit geändert (ChangedTime)
+           console.log(data.timetable.s[i].dp.$.ct);
 
            //Ziele der Abfahrt (Planned Path)
            console.log(data.timetable.s[i].dp.$.ppth);
 
 
 
-           // }
+            }
 
       }
 
